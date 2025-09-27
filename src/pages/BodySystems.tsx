47c4773be523d36ab34a2311ef57175620d3b929
Milap Patel <153003837+milappatel1@users.noreply.github.com>
2025-09-27 11:21:09 -0400

    Update BodySystems.tsx

--- src/pages/BodySystems.tsx
+++ src/pages/BodySystems.tsx
@@ -37,7 +37,7 @@ const BodySystems: React.FC = () => {
       </div>
 
       {/* Body Systems Grid */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
         {bodySystems.map((system) => {
           const IconComponent = getIconComponent(system.icon)
           const systemDiseases = getSystemDiseases(system.id)
